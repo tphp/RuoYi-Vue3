66b255d8eec2bde20db585a51f53080fc3d6fd34
yuxing <yuxing@superic.com>
2026-06-12 11:13:21 +0800

    版本管理，删除无关样式

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,10 @@ import { defineConfig, loadEnv } from 'vite'
 import path from 'path'
 import createVitePlugins from './vite/plugins'
 
-const baseUrl = 'http://localhost:8080' // 后端接口
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode, command }) => {
   const env = loadEnv(mode, process.cwd())
-  const { VITE_APP_ENV } = env
+  const { VITE_APP_ENV, VITE_APP_BASE_URI, VITE_APP_SERVER_PORT } = env
   return {
     // 部署生产环境和开发环境下的URL。
     // 默认情况下，vite 会假设你的应用是被部署在一个域名的根路径上
@@ -42,19 +40,19 @@ export default defineConfig(({ mode, command }) => {
     },
     // vite 相关配置
     server: {
-      port: 80,
+      port: VITE_APP_SERVER_PORT,
       host: true,
       open: true,
       proxy: {
         // https://cn.vitejs.dev/config/#server-proxy
         '/dev-api': {
-          target: baseUrl,
+          target: VITE_APP_BASE_URI,
           changeOrigin: true,
           rewrite: (p) => p.replace(/^\/dev-api/, '')
         },
          // springdoc proxy
          '^/v3/api-docs/(.*)': {
-          target: baseUrl,
+          target: VITE_APP_BASE_URI,
           changeOrigin: true,
         }
       }
